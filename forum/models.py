b865bcb6cb80043193f443b95eceb880bfa86aa7
Liene Briede <lienebriede84@gmail.com>
2024-06-06 07:22:11 +0000

    Create post model, register to admin

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Variable for approved posts
+STATUS = ((0, "Not Approved"), (1, "Approved"))
+
+class Post(models.Model):
+    """
+    Model for posts
+    """
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posts"
+        )
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
